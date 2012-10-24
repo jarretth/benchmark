3646951081ce18d615b975e2526c9792fe6202d9
Jarrett Hawrylak <jarrett.hawrylak@showclix.com>
2012-10-24 17:54:41 -0400

    apparently, i'm bad at this

--- resources/tests/BenchmarkTest.php
+++ resources/tests/BenchmarkTest.php
@@ -2,5 +2,8 @@
 use Benchmark\Benchmark;
 
 class BenchmarkTest extends PHPUnit_Framework_TestCase {
-
+    public function testStart() {
+        Benchmark::start('a');
+        $this->assertEquals(1,1);
+    }
 }
